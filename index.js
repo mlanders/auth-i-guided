baeea66bc990bca3a04e6f548a06e11bd08e6b60
Mike Landers <landers.mike@gmail.com>
2019-02-25 10:54:29 -0800

    final

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
@@ -12,49 +13,71 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+	res.send("It's alive!");
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
-  // generate hash from user's password
+	let user = req.body;
+	// generate hash from user's password
+	const hash = bcrypt.hashSync(user.password, 16);
+	// override user.password with hash
+	user.password = hash;
 
-  // override user.password with hash
-
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+	Users.add(user)
+		.then(saved => {
+			res.status(201).json(saved);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 });
 
 server.post('/api/login', (req, res) => {
-  let { username, password } = req.body;
+	let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      // check that passwords match
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+	Users.findBy({ username })
+		.first()
+		.then(user => {
+			// check that passwords match
+			if (user && bcrypt.compareSync(password, user.password)) {
+				res.status(200).json({ message: `Welcome ${user.username}!` });
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 });
 
+function restricted(req, res, next) {
+	let { username, password } = req.headers;
+
+	if (username && password) {
+		Users.findBy({ username })
+			.first()
+			.then(user => {
+				// check that passwords match
+				if (user && bcrypt.compareSync(password, user.password)) {
+					next();
+				} else {
+					res.status(401).json({ message: 'Invalid Credentials' });
+				}
+			})
+			.catch(error => {
+				res.status(500).json({ message: 'Unexpected error' });
+			});
+	} else {
+		res.status(400).json({ message: 'No credentials provided' });
+	}
+}
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+server.get('/api/users', restricted, (req, res) => {
+	Users.find()
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => res.send(err));
 });
 
 const port = process.env.PORT || 5000;

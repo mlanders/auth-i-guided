452999f07bebbfc4afce8b53db6ebbbce8e8e194
Mike Landers <landers.mike@gmail.com>
2019-02-26 10:57:30 -0800

    session and cookies code

--- index.js
+++ index.js
@@ -2,19 +2,68 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
 
 const server = express();
 
+const sessionConfig = {
+	name: 'walter', // name of session cookie
+	secret: process.env.SESSION_SECRET || 'this is a secret', // secret stored in .env file
+	cookie: {
+		maxAge: 1000 * 60 * 15, //in ms, seconds, min, hour, y
+		secure: false, // used over https only
+	},
+	httpOnly: true, //cannot access the cookie from js using document.cookie
+	resave: false, //
+	saveUninitialized: false, // laws agains setting cookies automatically
+	store: new KnexSessionStore({
+		knex: db,
+		tablename: 'sessions',
+		sidfieldname: 'sid', //session id field name
+		createtable: true,
+		clearInterval: 1000 * 60 * 60, // in ms
+	}),
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
 	res.send("It's alive!");
 });
+
+// ================================ Restricted Middleware
+function restricted(req, res, next) {
+	// let { username, password } = req.headers;
+
+	if (req.session && req.session.username) {
+		// if (username && password) {
+		// Users.findBy({ username })
+		// 	.first()
+		// 	.then(user => {
+		// 		// check that passwords match
+		// 		if (user && bcrypt.compareSync(password, user.password)) {
+		// 			next();
+		// 		} else {
+		// 			res.status(401).json({ message: 'Invalid Credentials' });
+		// 		}
+		// 	})
+		// 	.catch(error => {
+		// 		res.status(500).json({ message: 'Unexpected error' });
+		// 	});
+		next();
+	} else {
+		res.status(401).json({ message: 'You shall not pass' });
+	}
+}
+
+// ================================ Register
 
 server.post('/api/register', (req, res) => {
 	let user = req.body;
@@ -25,12 +74,16 @@ server.post('/api/register', (req, res) => {
 
 	Users.add(user)
 		.then(saved => {
+			req.session.username = saved;
+
 			res.status(201).json(saved);
 		})
 		.catch(error => {
 			res.status(500).json(error);
 		});
 });
+
+// ================================ Login
 
 server.post('/api/login', (req, res) => {
 	let { username, password } = req.body;
@@ -40,6 +93,7 @@ server.post('/api/login', (req, res) => {
 		.then(user => {
 			// check that passwords match
 			if (user && bcrypt.compareSync(password, user.password)) {
+				req.session.username = user.username;
 				res.status(200).json({ message: `Welcome ${user.username}!` });
 			} else {
 				res.status(401).json({ message: 'Invalid Credentials' });
@@ -50,28 +104,23 @@ server.post('/api/login', (req, res) => {
 		});
 });
 
-function restricted(req, res, next) {
-	let { username, password } = req.headers;
-
-	if (username && password) {
-		Users.findBy({ username })
-			.first()
-			.then(user => {
-				// check that passwords match
-				if (user && bcrypt.compareSync(password, user.password)) {
-					next();
-				} else {
-					res.status(401).json({ message: 'Invalid Credentials' });
-				}
-			})
-			.catch(error => {
-				res.status(500).json({ message: 'Unexpected error' });
-			});
+// ================================ Logout
+server.get('/api/logout', (req, res) => {
+	if (req.session) {
+		req.session.destroy(error => {
+			if (error) {
+				res.send('Error');
+			} else {
+				res.send('Successfully logged out');
+			}
+		});
 	} else {
-		res.status(400).json({ message: 'No credentials provided' });
+		res.end();
 	}
-}
+});
+
 // protect this route, only authenticated users should see it
+// ================================ Users (protected)
 server.get('/api/users', restricted, (req, res) => {
 	Users.find()
 		.then(users => {

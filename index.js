3324a23ae508ba24f8435438b7b13150ddb3d7d9
Mike Landers <landers.mike@gmail.com>
2019-02-27 09:29:30 -0800

    created notes file removed session code

--- index.js
+++ index.js
@@ -2,37 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const session = require('express-session');
-const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
 
 const server = express();
 
-const sessionConfig = {
-	name: 'walter', // name of session cookie
-	secret: process.env.SESSION_SECRET || 'this is a secret', // secret stored in .env file
-	cookie: {
-		maxAge: 1000 * 60 * 15, //in ms, seconds, min, hour, y
-		secure: false, // used over https only
-	},
-	httpOnly: true, //cannot access the cookie from js using document.cookie
-	resave: false, //
-	saveUninitialized: false, // laws agains setting cookies automatically
-	store: new KnexSessionStore({
-		knex: db,
-		tablename: 'sessions',
-		sidfieldname: 'sid', //session id field name
-		createtable: true,
-		clearInterval: 1000 * 60 * 60, // in ms
-	}),
-};
-
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
 	res.send("It's alive!");
@@ -40,27 +18,7 @@ server.get('/', (req, res) => {
 
 // ================================ Restricted Middleware
 function restricted(req, res, next) {
-	// let { username, password } = req.headers;
-
-	if (req.session && req.session.username) {
-		// if (username && password) {
-		// Users.findBy({ username })
-		// 	.first()
-		// 	.then(user => {
-		// 		// check that passwords match
-		// 		if (user && bcrypt.compareSync(password, user.password)) {
-		// 			next();
-		// 		} else {
-		// 			res.status(401).json({ message: 'Invalid Credentials' });
-		// 		}
-		// 	})
-		// 	.catch(error => {
-		// 		res.status(500).json({ message: 'Unexpected error' });
-		// 	});
-		next();
-	} else {
-		res.status(401).json({ message: 'You shall not pass' });
-	}
+	next();
 }
 
 // ================================ Register
@@ -74,8 +32,6 @@ server.post('/api/register', (req, res) => {
 
 	Users.add(user)
 		.then(saved => {
-			req.session.username = saved;
-
 			res.status(201).json(saved);
 		})
 		.catch(error => {
@@ -93,7 +49,6 @@ server.post('/api/login', (req, res) => {
 		.then(user => {
 			// check that passwords match
 			if (user && bcrypt.compareSync(password, user.password)) {
-				req.session.username = user.username;
 				res.status(200).json({ message: `Welcome ${user.username}!` });
 			} else {
 				res.status(401).json({ message: 'Invalid Credentials' });
@@ -102,21 +57,6 @@ server.post('/api/login', (req, res) => {
 		.catch(error => {
 			res.status(500).json(error);
 		});
-});
-
-// ================================ Logout
-server.get('/api/logout', (req, res) => {
-	if (req.session) {
-		req.session.destroy(error => {
-			if (error) {
-				res.send('Error');
-			} else {
-				res.send('Successfully logged out');
-			}
-		});
-	} else {
-		res.end();
-	}
 });
 
 // protect this route, only authenticated users should see it
---
+++ notes.md
@@ -0,0 +1,20 @@
+# notes
+
+-   The main difference between using sessions and tokens for auth: Where the state is being kept
+    -   Session keep state kept in the server
+        -   client (browser) sends it automatically
+    -   When using tokens tate is kept in the toek (client)
+        -   client is responsible for sending it back to server
+
+### Responsibilities
+
+-   Server
+    -   produce the token
+    -   send the token to the client
+    -   read, decode and verify the token
+    -   make the payload available to the rest of the api
+
+*   Client
+    -   store the token
+    -   send the token on every request
+    -   destroy token on logout
